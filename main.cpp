6100ebf8dfd8713d5dc4ad54c6cc997fc0b7a1b6
shanecarey17 <shanecarey17@gmail.com>
2015-12-13 15:16:31 -0600

    second commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPPong
+//
+//  Created by Shane Carey on 12/13/15.
+//  Copyright © 2015 Shane Carey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
